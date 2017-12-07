d0fd3a102c475797d8ea1fa11f537c6e1c1993a3
Tian Feng <tfeng@freewheel.tv>
2017-12-07 11:29:26 +0800

    update ckeditor setting

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module Wren
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    # Set ckeditor precompiled
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
